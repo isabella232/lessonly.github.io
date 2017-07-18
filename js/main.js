11f4a7f08f556dee78d76802e30dfc8e72fe0164
Steve Grossi <me@stevegrossi.com>
2017-07-18 13:40:00 -0400

    Come on, Steve, put the JS in a separate file

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Append links to markdown headings
+var anchorForId = function (id) {
+  var anchor = document.createElement("a");
+  anchor.className = "header-link";
+  anchor.href      = "#" + id;
+  anchor.innerHTML = "<i class=\"heading-link\">#</i>";
+  return anchor;
+};
+
+var linkifyAnchors = function (level, containingElement) {
+  var headers = containingElement.getElementsByTagName("h" + level);
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h];
+    console.log(header);
+
+    if (typeof header.id !== "undefined" && header.id !== "") {
+      header.appendChild(anchorForId(header.id));
+    }
+  }
+};
+
+document.onreadystatechange = function () {
+  if (this.readyState === "complete") {
+    for (var level = 2; level <= 6; level++) {
+      linkifyAnchors(level, document.body);
+    }
+  }
+};
